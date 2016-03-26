34525a5e44e299df0e88cdceece7ed581f1334e6
Cristian Fagarasan <office.cristif@gmail.com>
2016-03-26 13:06:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Salut Lume!");
+        System.out.println("Salut Lume1!");
+        System.out.println("Salut Lume2!");
+    }
+}
